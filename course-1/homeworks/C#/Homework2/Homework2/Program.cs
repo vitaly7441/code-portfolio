28980de70acc4f0e2c3b050ee09ec5b239b6d3ce
Vitaly Verchenko <jonmaklaun@gmail.com>
2025-11-27 19:01:47 +0300

    did 2 c# homework

---
+++ course-1/homeworks/C#/Homework2/Homework2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        //int tC = Convert.ToInt32(Console.ReadLine());
+        //double tF = tC * 9.0 / 5 + 32;
+        //Console.WriteLine(tF);
+
+
+        //double a = Convert.ToDouble(Console.ReadLine());
+        //double b = Convert.ToDouble(Console.ReadLine());
+        //double c = Convert.ToDouble(Console.ReadLine());
+        //double arithMean = Math.Round((a + b + c) / 3, 2);
+        //Console.WriteLine($"Среднее арифмитическое = {arithMean}, оно вычисляется по формуле: (a+b+c)/3 и округляется до 2 знаков после запятой");
+
+
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine(a + b);
+        //Console.WriteLine(a - b);
+        //Console.WriteLine(a * b);
+        //Console.WriteLine(a / b);
+
+    }
+}
---
+++ course-1/practices/Practice1/Practice1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //int a = 12, b = 5;
+        //Console.WriteLine(a + b);
+        //Console.WriteLine(a - b);
+        //Console.WriteLine(a * b);
+        //Console.WriteLine(a % b);
+
+        //string? name = Console.ReadLine();
+        //Console.WriteLine("Привет, "+name);
+
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine(a + b);
+
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine(a * b);
+
+        //int number = -10;
+        //if (number > 0)
+        //{
+        //    Console.WriteLine("Число положительное");
+        //}
+        //else if (number < 0)
+        //{
+        //    Console.WriteLine("Число отрицательное");
+        //}
+        //else {
+        //    Console.WriteLine("Число равно нулю");
+        //}
+
+        //int age = Convert.ToInt32(Console.ReadLine());
+        //if (age >= 18)
+        //{
+        //    Console.WriteLine("Вы совершеннолетний");
+        //}
+        //else {
+        //    Console.WriteLine("Вы не совершеннолетний");
+        //}
+
+        //int a = 5, b = -2;
+        //if (a > 0 && b > 0)
+        //{
+        //    Console.WriteLine("Оба числа положительные");
+        //}
+        //else if (a > 0 || b > 0)
+        //{
+        //    Console.WriteLine("Хотя бы одно число положительное");
+        //}
+        //else if (a < 0) {
+        //    Console.WriteLine("a не положительное");
+        //}
+
+        //int grade = Convert.ToInt32(Console.ReadLine());
+        //if (grade < 3)
+        //{
+        //    Console.WriteLine("Неудовлетворительно");
+        //}
+        //else if (grade == 3)
+        //{
+        //    Console.WriteLine("Удовлетворительно");
+        //}
+        //else if (grade == 4) {
+        //    Console.WriteLine("Хорошо");
+        //}
+        //else if (grade == 5)
+        //{
+        //    Console.WriteLine("Отлично");
+        //}
+    }
+}
