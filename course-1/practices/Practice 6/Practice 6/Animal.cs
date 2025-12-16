c9de974935c265f50427bfd84175b8fbba9eb306
Vitaly Verchenko <jonmaklaun@gmail.com>
2025-12-16 18:53:43 +0300

    add practice 6

---
+++ course-1/practices/Practice 6/Practice 6/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Practice_6
+{
+	public class Animal
+	{
+        protected string name;
+        protected int age;
+        protected string color;
+
+		public Animal (string name, int age, string color) {
+			this.name = name;
+			this.age = age;
+			this.color = color;
+		}
+
+		public virtual void MakeSound() {
+			Console.WriteLine("Животное издаёт звук");
+		}
+	}
+}
+
---
+++ course-1/practices/Practice 6/Practice 6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_6 {
+    class Program
+    {
+        static void Main()
+        {
+            Dog dog = new Dog("Name1", 13, "red");
+            dog.MakeSound();
+        }
+    }
+}
