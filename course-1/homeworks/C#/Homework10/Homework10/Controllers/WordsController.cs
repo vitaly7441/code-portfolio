300234bdca99e7fd3cfc3c22274c1a0a92255bbb
Vitaly Verchenko <jonmaklaun@gmail.com>
2026-04-06 22:57:58 +0300

    Add homework 10

---
+++ course-1/homeworks/C#/Homework10/Homework10/Controllers/WordsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Homework10.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WordsController : ControllerBase
+{
+    private static List<string> words = new List<string>() {
+        "Word1",
+        "Word2",
+        "Word3"
+    };
+
+    [HttpGet]
+    public IActionResult GetAllWords()
+    {
+        foreach (var i in words) {
+            Console.WriteLine(i);
+        }
+        return Ok(words);
+    }
+
+    [HttpPost]
+    public IActionResult AddWord(string word)
+    {
+        words.Add(word);
+        return Ok(words);
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteWord(int index)
+    {
+        words.Remove(words[index]);
+        return Ok(words);
+    }
+
+
+}
+
